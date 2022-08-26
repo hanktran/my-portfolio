06d9179bbfb76c163167aabcdcf706fbe52c5b2e
Han Tran <imhantran@gmail.com>
2022-08-26 08:15:49 +0700

    update layout

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -58,7 +58,7 @@ const Header = () => (
       whileInView={scaleVariants.whileInView}
       className="app__header-circles"
     >
-      {[images.react, images.node, images.typescript].map((circle, index) => (
+      {[images.react, images.mongodb, images.node].map((circle, index) => (
         <div className="circle-cmp app__flex" key={`circle-${index}`}>
           <img src={circle} alt="profile_bg" />
         </div>
